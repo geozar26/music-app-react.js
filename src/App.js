f501fc27ec2d3c3124d1c9584476dc98acfd3ff7
george zareifhs <giorgoszar67@gmail.com>
2026-02-05 16:27:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -169,8 +169,8 @@ const MusicApp = () => {
           </div>
 
           <div className="flex items-center gap-6 pr-12">
-            <button className="text-[15px] font-bold capitalize text-zinc-400 hover:text-hover:text-[#6366f1] transition-colors">Install</button>
-            <button className="text-[15px] font-bold capitalize  text-zinc-400hover:text-[#6366f1]  transition-colors">Log In</button>
+            <button className="text-[15px] font-bold capitalize text-zinc-400 hover:text-[#6366f1] transition-colors">Install</button>
+            <button className="text-[15px] font-bold capitalize  text-zinc-400hover:text-[#6366f1] transition-colors">Log In</button>
             <button className="bg-white text-black text-[14px] font-black capitalize px-6 py-2 rounded-full border border-transparent hover:bg-transparent hover:border-[#6366f1] hover:text-[#6366f1] transition-all duration-300">
               Sign Up
             </button>
